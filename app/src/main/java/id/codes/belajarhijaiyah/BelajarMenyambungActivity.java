b202553570d30a57d528cee8f1ce7a3530088c2d
wildan romiza <38659848+wildanelek12@users.noreply.github.com>
2022-08-18 14:47:12 +0700

    Initial commit

---
+++ app/src/main/java/id/codes/belajarhijaiyah/BelajarMenyambungActivity.java
@@ -0,0 +1,14 @@
+package id.codes.belajarhijaiyah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BelajarMenyambungActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_belajar_menyambung);
+    }
+}
---
+++ app/src/main/java/id/codes/belajarhijaiyah/MainActivity.java
@@ -0,0 +1,24 @@
+package id.codes.belajarhijaiyah;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHijaiyah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+
+    }
+}
